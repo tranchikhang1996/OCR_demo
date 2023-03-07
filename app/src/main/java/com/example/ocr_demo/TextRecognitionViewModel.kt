9fc99c699919db74ee939b03a1defef363fb7e48
khangtc2 <khangtc2@vng.com.vn>
2023-03-07 15:34:06 +0700

    integrate ML kit

--- app/src/main/java/com/example/ocr_demo/TextRecognitionViewModel.kt
+++ app/src/main/java/com/example/ocr_demo/TextRecognitionViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class TextRecognitionViewModel() : ViewModel() {
     private val _result = MutableLiveData<String>()
     val result: LiveData<String> = _result
+    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
     fun image2Text(input: InputImage) {
+        recognizer.process(input).addOnSuccessListener {
+            _result.postValue(it.text)
+        }
     }
 }
