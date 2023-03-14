dc2d11e630b0bad2bdfdac8edab9a5cb5a3dc3e5
khangtc2 <khangtc2@vng.com.vn>
2023-03-14 16:07:40 +0700

    with out text recognition library

--- app/src/main/java/com/example/ocr_demo/TextRecognitionViewModel.kt
+++ app/src/main/java/com/example/ocr_demo/TextRecognitionViewModel.kt
@@ -1,58 +1,26 @@
 package com.example.ocr_demo
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.*
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.File
 
 
 class TextRecognitionViewModel(context: Context) : ViewModel() {
     private val _result = MutableLiveData<String>()
     val result: LiveData<String> = _result
-    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    private val testApi: TessBaseAPI
 
-    init {
-        val dataPath: String = File(context.getExternalFilesDir(null), "tesseract").absolutePath
-        testApi = TessBaseAPI()
-        if (!testApi.init(dataPath, "vie")) {
-            testApi.recycle()
-        }
-    }
-
-    fun image2text(input: InputImage, engine: String) {
+    fun image2text(engine: String) {
         when (engine) {
-            "ML-kit" -> mlKit(input)
-            "Tesseract" -> tesseract(input)
+            "ML-kit" -> mlKit()
+            "Tesseract" -> tesseract()
         }
     }
 
-    private fun mlKit(input: InputImage) {
-        recognizer.process(input).addOnSuccessListener {
-            _result.postValue(it.text)
-        }
-    }
+    private fun mlKit() {}
 
-    private fun tesseract(inputImage: InputImage) {
-        viewModelScope.launch(Dispatchers.Default) {
-            try {
-                testApi.setImage(inputImage.bitmapInternal)
-                testApi.utF8Text?.let { _result.postValue(it) }
-            } catch (throwable: Throwable) {
-                Log.d("ERROR", throwable.message ?: "")
-            }
-        }
-    }
-
-    override fun onCleared() {
-        testApi.recycle()
-        super.onCleared()
+    private fun tesseract() {
+        viewModelScope.launch(Dispatchers.Default) {}
     }
 
     class Factory(private val context: Context) : ViewModelProvider.Factory {
